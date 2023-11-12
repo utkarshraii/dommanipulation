766e92421fe79316d01886fa3f87e1bf0cdf02d6
utkarshraii <utkarshr348@gmail.com>
2023-11-12 19:11:44 +0530

    added index.html with proper bootstap link

---
+++ src/dom.js
@@ -0,0 +1,16 @@
+// Document object model
+// tree of nodes/elements created by browser
+// javascript can be used to read/write/manipulate to the DOM
+// object oriented representation
+// TREE STRUCTURE
+//                 document
+//                 |
+//             root element(html)
+//             /                \
+//            /                  \
+//         element(head)       element(body)
+//             |                 /         \
+//         element(title)       /           \
+//             |             element<a>   element<h1>
+//         text-"my title"      |             |
+//                             text-"my link"    text-"my header"
